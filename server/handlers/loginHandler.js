c98075a0a254b061d9dc6f29805d515e06b9888d
RaulDiaz <rulhdiaz@gmail.com>
2024-04-05 16:18:23 -0600

    commit

--- server/handlers/loginHandler.js
+++ server/handlers/loginHandler.js
@@ -52,7 +52,34 @@ const googleSignInHandler = async (req, res = response) => {
 
   try {
     const googleUser = await googleVerify(google_token);
-    //console.log(googleUser)
+    console.log(googleUser);
+    const { email, name, picture } = googleUser;
+
+    let usuario = await User.findOne({where:{email: email}});
+
+    // if usuario no existe crearlo con datos de google
+    if(!usuario){
+      const data = {
+        username: name, 
+        email: email,
+        password: ":P",
+        picture,
+        google: true
+      };
+
+      usuario = await User.create( data );
+    }
+
+    if(!usuario.active){
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado.'
+      });
+    }
+
+    // generate JWT
+    const token = await generarJWT(usuario.id);
+
+    // if usuario ya existe con datos de google entonces todo bien
     res.json({
       msg: 'Todo bien!',
       google_token,
